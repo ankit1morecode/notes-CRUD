8191b57159f604ead913ad12cd93cebb720831bb
ankit1morecode <ankitsafeid@gmail.com>
2026-06-04 16:01:33 +0530

    add git-ignore

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,8 @@
 let mongoose = require("mongoose");
+require('dotenv').config();
 
 async function connectDB() {
-    await mongoose.connect("mongodb+srv://backend:d3RtNfGeSdfxOVVs@cluster0.pzcpzii.mongodb.net/notes-db");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("connected to DB");
 }
 
